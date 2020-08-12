199028bc38d646c2613c298e72bb5fc2890f67ae
shazy89 <erdo.shazy123@gmail.com>
2020-08-12 13:42:57 -0400

    Done.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :github, 'd9f1804bfafd00e9b4ad', '2623210b81cd862d0d97a99764d43403b177dab4'
+    provider :github, ENV['d9f1804bfafd00e9b4ad'], ENV['2623210b81cd862d0d97a99764d43403b177dab4']
 
   end
